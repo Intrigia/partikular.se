48357901b4fca8db081b9d2b723379169dd67465
Karl Sellergren <karl.sellergren@partikular.se>
2021-04-01 21:49:26 +0200

    Remove ignored files

--- wp-content/plugins/radio-buttons-for-taxonomies/js/dist/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '00e1c63dbb454faf84f06226539c62ae');
--- wp-content/plugins/white-label-cms/view/admin/parts/login-live-preview.php
+++
@@ -1,6 +0,0 @@
-<div class="lightbox short-animate" id="wlcms-login">
-    
-</div>
-<div id="lightbox-controls" class="short-animate">
-    <a id="close-lightbox" class="long-animate" href="#login"><?php _e('Close Login')?></a>
-</div>
